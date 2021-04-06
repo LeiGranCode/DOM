387035d094817a5fb64fae05c2593f4a2e4385e7
leigrancode <leiver.g.c@hotmail.es>
2021-04-06 13:07:57 -0500

    Primera practica de DOM con nodos

--- src/index.js
+++ src/index.js
@@ -3,4 +3,32 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url="https://platzi-avo.vercel.app/api/avo";
+
+//web api
+//conecctarnos al server
+window
+.fetch(url)
+//procesar la respuesta y convertirla en JSON
+.then((respuesta)=>respuesta.json())
+//JSON -> Data -> renderizar info browser
+.then((responseJson)=>{
+  const  todosLosItems=[]
+
+  responseJson.data.forEach(item => {
+    //crear imagen
+    const imagen=document.createElement("img");
+    document.body.appendChild(imagen);
+    //crear titulo
+    const title=document.createElement("h2");
+    document.body.appendChild(title);
+    //crear precio
+    const price=document.createElement("div");
+    document.body.appendChild(price);
+
+    const container= document.createElement('div')
+    container.append(imagen,title,price);
+    todosLosItems.push(container)
+  });
+  document.body.append(...todosLosItems);
+});
