1d7d34af6d979631c2a441d6be0ca822c05b4a21
leigrancode <leiver.g.c@hotmail.es>
2021-04-06 14:54:41 -0500

    Uso de tailwindcss para mejorar la pag

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 const baseUrl ="https://platzi-avo.vercel.app";
-const appNode= document.querySelector('#app')
+const appNode= document.querySelector('#app');
+const formatPrice=(price)=>{
+  const newPrice=new window.Intl.NumberFormat('en-EN', {
+    style:"currency",
+    currency:"USD"
+  }).format(price);
+  return newPrice;
+}
 //web api
 //conecctarnos al server
 window
@@ -18,12 +25,15 @@ window
     //crear imagen
     const imagen=document.createElement("img");
     imagen.src=`${baseUrl}${item.image}`;
+    imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
     //crear titulo
     const title=document.createElement("h2");
     title.textContent=item.name;
+    title.className='text-2xl text-green-600';
     //crear precio
     const price=document.createElement("div");
-    price.textContent=item.price;
+    price.textContent=formatPrice(item.price);
+    price.className='text-blue-600'
 
     const container= document.createElement('div')
     container.append(imagen,title,price);
